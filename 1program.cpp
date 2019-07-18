b1c4bc87ba94bcf25418ca3f26d4cb9002c1505a
maafia007 <53034066+maafia007@users.noreply.github.com>
2019-07-18 12:29:23 +0530

    Create 1program.cpp

---
+++ 1program.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+#include<conio.h>
+void main()
+{
+cout<<"Welcome to the Horizon";
+int a,b,c, sum;
+cout<<"Enter the values of  integers"
+cin>>a>>b>>c;
+sum=(a+b+c);
+cout<<"The Sum of the integers is/n"<<sum;
+getch();
+}
